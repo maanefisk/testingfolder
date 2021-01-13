2b6e6c99ad1a141bcbf1a1ce90d89435f9052f23
maanefisk <62015771+maanefisk@users.noreply.github.com>
2021-01-13 16:31:34 +0100

    Css added, figured how to make a variable with some kind of calculation

--- test2.php
+++ test2.php
@@ -1,10 +1,19 @@
+<html>
+<head><link rel="stylesheet" href="stylesheet.css"><head>
+<body>
+<br>
+<div class="buttoncontainer"><button>New Character</button><button>Fight!</button></div>
+<br>
+<div class="fightarena">
+    <div class="characterslot">Characterslot</div>
+    <div class="enemyslot">Enemyslot</div>
+    <div class="fightaction">Fightaction</div>
+</div>
+
+</body>
+</html>
+
 <?php
-class Character {
-    public $name = '';
-    public $title = '';
-    public $weapon = array();
-    public $HP = 0;
-}
 
 class Sword {
     public $damage = 120;
@@ -27,37 +36,37 @@ class Spreadterror {
 class Faramir {
     public $name = 'Faramir';
     public $title = 'Captain';
-    public $weapon = array('sword','bow');
+    public $weapon = array('Sword','Bow');
     public $HP = 800;
 }
 class Sam {
     public $name = 'Sam';
     public $title = 'Mayor';
-    public $weapon = array('sword','shout');
+    public $weapon = array('Sword','Shout');
     public $HP = 500;
 }
 class Galadriel {
     public $name = 'Galadriel';
     public $title = 'Lady';
-    public $weapon = array('magicdamage','telepathy');
+    public $weapon = array('Magicdamage','Telepathy');
     public $HP = 2000;
 }
 class Nazgul {
     public $name = 'Nazgul';
     public $title = 'Ringwraith';
-    public $weapon = array('sword','spreadterror');
+    public $weapon = array('Sword','Spreadterror');
     public $HP = 1000;
 }
 class UrukHai {
     public $name = 'Uruk-Hai';
     public $title = 'Commander';
-    public $weapon = array('sword','bow');
+    public $weapon = array('Sword','Bow');
     public $HP = 600;
 }
 class Orc {
     public $name = 'Orc';
     public $title = 'Warrior';
-    public $weapon = array('sword','bow');
+    public $weapon = array('Sword','Bow');
     public $HP = 400;
 }
 //object of class is declared
@@ -67,29 +76,24 @@ $Galadriel = new Galadriel();
 $Nazgul = new Nazgul();
 $UrukHai = new UrukHai();
 $Orc = new Orc();
+$Sword = new Sword();
+$Bow = new Bow();
+$Shout = new Shout();
+$Magicdamage = new Magicdamage();
+$Telepathy = new Telepathy();
+$Spreadterror = new Spreadterror();
 
 $goodOnes = array($Faramir, $Sam, $Galadriel);
 $badOnes = array($Nazgul, $UrukHai, $Orc);
+
+$randomcharacter = $goodOnes[rand(0,2)]->name;
 //properties of class Bird are accessed using object
 echo '<br>  A warrior =  '.$Faramir->name;
 echo '<br>  Faramirs random weapon = '.$Faramir->weapon[rand(0,1)];
 echo '<br>  One of the Good Ones =  '.$goodOnes[rand(0,2)]->name;
-echo '<br>  Good Ones Weapons = '.$goodOnes[rand(0,2)]->weapon[rand(0,1)];
+echo '<br>  One of the Good Ones Weapons = '.$goodOnes[rand(0,2)]->weapon[rand(0,1)];
 echo '<br>  One of the Bad Ones HP = '.$badOnes[rand(0,2)]->HP;
 echo '<br>  One of the Good Ones =  '.$goodOnes[rand(0,2)]->name;
-echo '<br>  Get a character =', '<script type="text/javascript">', 'characterChooser();', '</script>';
+echo '<br>'.$randomcharacter;
 
 ?>
-
-<button onclick="characterChooser()">Click Here</button>
-<script>
-    function characterChooser(){
-        let activeCharacter = <?=$goodOnes[rand(0,2)]->name?> ;
-        let activeCharacter2 = 'testo' ;
-        return activeCharacter2;
-        console.log(activeCharacter2);
-    }
-    function weaponChooser(){
-        let weapon = <?=$lang->get('zipcode')?>;
-    }
-</script>
